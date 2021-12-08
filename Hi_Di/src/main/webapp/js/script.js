f87bf19aa033915ba1d4ae3618fa2ca1285c55dd
smhrd <smhrd@DESKTOP-6VRK9A5>
2021-12-08 14:18:31 +0900

    프로젝트 파일

---
+++ Hi_Di/src/main/webapp/js/script.js
@@ -0,0 +1,4 @@
+function img(){
+    // 클릭했을때 img태그의 src값을 수정하는 부분
+    document.getElementsByClassName("slider-homez").src = "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMDEyMTJfMjQy%2FMDAxNjA3NzU4MzM2MTA2.ojXKk6VOfMDnw-soMbPmyRXeVoeZPDeiEqGYtVi39AIg.kOx3iHeN-uf4hRmzHKzhAzDAXRq4w89h_mKEfHulR4Qg.JPEG.smotherguy%2FPartyU_mobile_3.jpg&type=ofullfill340_600";
+}
